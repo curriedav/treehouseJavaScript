7fdb8543b718a1d8322222c7186a6a7eaebe9cbe
curriedav <curriedav@gmail.com>
2013-10-07 14:24:43 -0700

    adds lesson 1, 2, & 3 files

---
+++ progammingIntro/myScriptL2.js
@@ -0,0 +1,36 @@
+//Lesson 2 of Intro to Programming
+
+/*
+console.log("Before");
+
+if (true) {
+	console.log("IF block")
+} else {
+	console.log("ELSE block")
+};
+
+console.log("After");
+*/
+
+console.log("Before");
+
+/*
+var counter = 10;
+
+
+while (counter) {
+	console.log("Hello World!");
+	counter = counter - 1;
+};
+
+while (prompt(What is your name?)) {
+	console.log("Got your name!");
+	counter = counter - 1;
+};
+*/
+
+for ( var counter = 10; counter; counter = counter - 1) {
+	console.log("Hello World!", counter)
+};
+
+console.log("After");
