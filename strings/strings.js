6864d5eefa90eaea6c4b722b338ae0fb4836b5c3
curriedav <curriedav@gmail.com>
2013-10-15 18:00:36 -0700

    string variable lesson

---
+++ strings/strings.js
@@ -0,0 +1,34 @@
+/* Javascript Strings */
+
+var name = "David";
+console.log(name);
+
+var name2 = 'David';
+console.log(name2);
+
+var statement = "This is David's string.";
+console.log(statement);
+
+var statement = "This is David's string.";
+console.log(statement);
+
+var statement2 = 'He said "this is awesome!';
+console.log(statement2);
+
+var statement3 = "He said \"this is David's string.\"";
+console.log(statement3);
+
+var path = "C:\\\\folder\\myfile.txt";
+console.log(path);
+
+var multiLine = "This is line one\n\tThis is line two\n\t\tThis is line three";
+console.log(multiLine);
+
+
+
+
+
+
+
+
+
