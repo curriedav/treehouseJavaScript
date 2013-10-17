64d2f22096c1d10cbe93d2a210cd453ac906350b
curriedav <curriedav@gmail.com>
2013-10-17 13:15:00 -0700

    finishes string lessons

--- strings/strings.js
+++ strings/strings.js
@@ -24,11 +24,36 @@ console.log(path);
 var multiLine = "This is line one\n\tThis is line two\n\t\tThis is line three";
 console.log(multiLine);
 
+console.log(multiLine.length);
 
+console.log(multiLine[5]);
+console.log(multiLine.charAt(5));
+console.log(multiLine.substr(8, 4).toUpperCase());
 
+var first = "Hello";
+var second = "hello";
 
+if (first.toLowerCase() === second.toLowerCase()) {
+	console.log("These strings are equal! \t :)");
+} else { 
+	console.log("These strings are not equal. \t :(");
+};
 
+function compare(a, b) {
+	console.log(a + " < " + b, a < b);
+}
 
+compare('a', 'b');
 
+compare('a', 'A');
 
+compare('apples', 'oranges');
+
+compare('apples', 'applications');
+
+compare('app', 'apples');
+
+compare('apples', 'app');
+
+compare('hello', 'hello');
 
